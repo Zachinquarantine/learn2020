5a02104d6a9a11c658b7bbf054b6fa8226861252
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2020-12-24 15:18:37 -0500

    Update code.py

--- code.py
+++ code.py
@@ -8,32 +8,24 @@ class Lexer(Lexer):
 				'*', '(', ')', ',', ';'} 
 
 
-	# Define tokens as regular expressions 
-	# (stored as raw strings) 
 	NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 	STRING = r'\".*?\"'
 
-	# Number token 
 	@_(r'\d+') 
 	def NUMBER(self, t): 
-		
-		# convert it into a python integer 
+
 		t.value = int(t.value) 
 		return t 
 
-	# Comment token 
 	@_(r'//.*') 
 	def COMMENT(self, t): 
 		pass
 
-	# Newline token(used only for showing 
-	# errors in new line) 
 	@_(r'\n+') 
 	def newline(self, t): 
 		self.lineno = t.value.count('\n')
 
 class Parser(Parser): 
-	#tokens are passed from lexer to parser 
 	tokens = Lexer.tokens 
 
 	precedence = ( 
